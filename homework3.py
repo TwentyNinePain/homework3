9fb52e7db2b6d4ebf3a48ca947a4f193606e831f
TwentyNinePain <nagatopainofrainvillage@gmail.com>
2024-05-24 22:03:49 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Максим'
+print('Имя: ', name, type(name))
+age = 19
+print('Возраст: ', age, type(age))
+age = age+2
+print('Новый возраст: ', age, type(age))
+id_student = True
+print('Является ли студентом: ', id_student, type(id_student))
